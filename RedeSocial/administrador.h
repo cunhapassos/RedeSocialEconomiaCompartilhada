207082252ef692077dcd6e3ba711dc6f57d7b6f2
NurCorezzi <nurcorezzi@hotmail.com>
2016-12-16 23:00:59 -0200

    Update administrador.h

--- RedeSocial/administrador.h
+++ RedeSocial/administrador.h
@@ -1,3 +1,22 @@
+/** ********************************************************************************
+ *  @addtogroup MODULO REDE_SOCIAL
+ *  @{
+ *  @ingroup MOD_RS
+ *
+ *  @file Pessoa.h
+ *  @brief Módulo de definição: Módulo Rede Social:
+ *         Implementa a Rede Social de Economia Compartilhada por meio da estrutura
+ *         de dados de umm grafo. Os pessoas da rede são representadas pelos vertices do gra-
+ *         fo e os relacionamentos são representados pelas arestas.
+ *         O grafo é implementado por meio de lista duplamente encadeada.
+ *
+ * @author NUR CORREIA - MAT. 15/0143290
+ * @author PAULO DA CUNHA PASSOS - MAT. 10/0118577
+ * @version 1.0
+ * @since 19/11/2016
+ *
+ @}********************************************************************************/
+
 #ifndef ADMINISTRADOR_H
 #define ADMINISTRADOR_H
 
@@ -7,6 +26,24 @@ namespace Ui {
 class Administrador;
 }
 
+/** ********************************************************************************
+ * Classe: Administrador
+ *
+ * Descrição da classe
+ *  A classe Administrador é responsavel por armazenar uma pessoa a qual será o 
+ * administrador da rede e fazer a integração com a interface gráfica.
+ *
+ * Requisitos assegurados pela classe:
+ *  Armazenagem de dados pessoais assim como os protocolos necessários para a 
+ * comunicação com a interface gráfica.
+ *
+ * Hipóteses assumidas pela classe:
+ *  A classe ainda não faz exigencias quanto a seu uso.
+ *
+ * Restrições da classe:
+ *  A classe ainda não possui restrições.  
+ *
+ * ********************************************************************************/
 class Administrador : public QDialog
 {
     Q_OBJECT
