538ce220742d65bc04a9e3e2e6f0222c97b39fa3
NurCorezzi <nurcorezzi@hotmail.com>
2016-12-16 22:55:24 -0200

    Update usuario.h

--- RedeSocial/usuario.h
+++ RedeSocial/usuario.h
@@ -1,3 +1,22 @@
+/** ********************************************************************************
+ *  @addtogroup MODULO REDE_SOCIAL
+ *  @{
+ *  @ingroup MOD_RS
+ *
+ *  @file Pessoa.h
+ *  @brief Módulo de definição: Módulo Rede Social:
+ *         Implementa a Rede Social de Economia Compartilhada por meio da estrutura
+ *         de dados de umm grafo. Os pessoas da rede são representadas pelos vertices do gra-
+ *         fo e os relacionamentos são representados pelas arestas.
+ *         O grafo é implementado por meio de lista duplamente encadeada.
+ *
+ * @author NUR CORREIA - MAT. 15/0143290
+ * @author PAULO DA CUNHA PASSOS - MAT. 10/0118577
+ * @version 1.0
+ * @since 19/11/2016
+ *
+ @}********************************************************************************/
+
 #ifndef USUARIO_H
 #define USUARIO_H
 
@@ -6,6 +25,24 @@
 #include "editarperfil.h"
 #include "transacoesjanela.h"
 
+/** ********************************************************************************
+ * Classe: Usuario
+ *
+ * Descrição da classe
+ *  Armazena uma pessoa como usuario para integração com a interface gráfica.
+ *
+ * Requisitos assegurados pela classe:
+ *  Armazenagem dos dados de uma pessoa assim como os protocolos para comunicação
+ * com a interface gráfica.
+ *
+ * Hipóteses assumidas pela classe:
+ *  Formatação de termos corretamento inseridos pelo usuário, a principio módulo não
+ * faz validação de campos.
+ *
+ * Restrições da classe:
+ *  É necessário o iso de formatos válidos como assertiva de entrada.
+ *
+ * ********************************************************************************/
 namespace Ui {
 class Usuario;
 }
@@ -16,16 +53,43 @@ class Usuario : public QMainWindow
 
 public:
     QMainWindow *main;
-
+    
+    /** ******************************************************************************
+      *  Funcao: listarPessoasDaRede
+      *
+      *  Descricao da funcao:
+      *      Lista diretamente para interface gráfica todas as pessoas peretencentes a
+      * a rede.
+      * 
+      ** *******************************************************************************/
     void listarPessoasDaRede();
+    
+    
+    /** ******************************************************************************
+      *  Funcao: listarAmigos
+      *
+      *  Descricao da funcao:
+      *      Lista diretamente para interface gráfica todas as pessoas em relacionamentos 
+      * com a pessoa a qual se refere o usuario.
+      * 
+      ** *******************************************************************************/
     void listarAmigos();
-   // void listarInteresses();
+    
+     /** ******************************************************************************
+      *  Funcao: listarRelacionamentos
+      *
+      *  Descricao da funcao:
+      *      Lista diretamente para interface gráfica todos os relacionamentos em que 
+      * esta pessoa se encontra, informado destino e origem.
+      * 
+      ** *******************************************************************************/
     void listarRelacionamentos();
     explicit Usuario(QWidget *parent = 0, Rede *_rede = 0, Pessoa *_pes = 0);
     void setUsuario(Pessoa *usuario);
     ~Usuario();
 
 private slots:
+
     void on_actionEditar_Perfil_triggered();
 
     void on_actionExcluir_Perfil_triggered();
